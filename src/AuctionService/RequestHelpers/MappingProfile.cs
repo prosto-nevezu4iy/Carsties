490b1e1c38684d295f9a56bb0f1d28405b8d3cab
Alexandru.Rotaru <Alexandru.Rotaru@qinshift.com>
2024-12-03 11:35:24 +0200

    End of section 4

--- src/AuctionService/RequestHelpers/MappingProfile.cs
+++ src/AuctionService/RequestHelpers/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using Contracts;
 
 namespace AuctionService.RequestHelpers;
 
@@ -11,7 +12,10 @@ public class MappingProfile : Profile
         CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
         CreateMap<Item, AuctionDto>();
         CreateMap<CreateAuctionDto, Auction>()
-            .ForMember(d => d.Item, opt => opt.MapFrom(s => s));
-        CreateMap<CreateAuctionDto, Item>();        
+            .ForMember(d => d.Item, o => o.MapFrom(s => s));
+        CreateMap<CreateAuctionDto, Item>();
+        CreateMap<AuctionDto, AuctionCreated>();
+        CreateMap<Auction, AuctionUpdated>().IncludeMembers(a => a.Item);
+        CreateMap<Item, AuctionUpdated>();
     }
 }
---
+++ src/SearchService/RequestHelpers/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Contracts;
+using SearchService.Models;
+
+namespace SearchService.RequestHelpers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<AuctionCreated, Item>();
+        CreateMap<AuctionUpdated, Item>();
+    }
+}
